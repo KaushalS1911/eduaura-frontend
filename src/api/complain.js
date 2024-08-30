1b35c6c8cab86fd813998487f2cb0e23064e9562
sujalpaghadal <sujalpaghadal17@gmail.com>
2024-08-30 13:51:04 +0530

    title name change

--- src/api/complain.js
+++ src/api/complain.js
@@ -8,7 +8,7 @@ import { fetcher } from '../utils/axios';
 export function useGetComplaints() {
   const { user } = useAuthContext();
 
-  const URL = `https://admin-panel-dmawv.ondigitalocean.app/api/v2/66710d4da24c98a090efd336/complain`;
+  const URL = `https://admin-panel-dmawv.ondigitalocean.app/api/v2/${user?.company_id}/complain`;
 
   const { data, isLoading, error, isValidating, mutate } = useSWR(URL, fetcher);
 
